ac2f7bff6b605757151226fbd4be3f887b2981d0
David Satrio <david.agung.satrio@gmail.com>
2025-09-17 03:34:25 +0700

    Quartz sync: Sep 17, 2025, 3:34 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
       }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "relative", openLinksInNewTab: true, lazyLoad: true}),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest", openLinksInNewTab: true, lazyLoad: true}),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true, callout: true, }),
